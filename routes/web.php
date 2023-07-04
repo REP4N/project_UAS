fdd4250a9414f8516e21d193a412b81237fbb8fa
REP4N <201220041repan@gmail.com>
2023-07-04 22:07:52 +0700

    perubahan

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard/index');
+Route::get('/admin', function () {
+    return view('admin/dashboard_admin');
 });
