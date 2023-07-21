8bedb13e6025f24e4af5313811dc8e9cdeb879c6
REP4N <201220041repan@gmail.com>
2023-07-21 14:49:53 +0700

    up

--- resources/views/dashboard/body/footer.blade.php
+++ resources/views/dashboard/body/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer-admin mt-auto footer-light">
+{{-- <footer class="footer-admin mt-auto footer-light">
     <div class="container-xl px-4">
         <div class="row">
             <div class="col-md-6 small">Copyright © Your Website <script>document.write(new Date().getFullYear())</script></div>
@@ -9,4 +9,4 @@
             </div>
         </div>
     </div>
-</footer>
+</footer> --}}
