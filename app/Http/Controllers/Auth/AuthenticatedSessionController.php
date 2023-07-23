9841565d8da45d0f9415e7634ed67d82c51be806
REP4N <201220041repan@gmail.com>
2023-07-24 00:15:36 +0700

    up

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -1,14 +1,13 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
-
 class AuthenticatedSessionController extends Controller
 {
     /**
@@ -17,34 +16,18 @@ class AuthenticatedSessionController extends Controller
     public function create(): View
     {
         return view('auth.login');
-    }  
-
-    //public function login (Request $request) {
-       // $credentials = $request->validate([
-            //'email' => 'required|email:dns',
-          //  'password' => 'required'
-        //]
-
-       // );
-
-       // if(Auth::attempt($credentials))
-        //{
-           // $request->session()->regenerate();
-
-           // return redirect()->intended(footer);
-        //}
-    //}
-
+    }
     /**
      * Handle an incoming authentication request.
      */
     public function store(LoginRequest $request): RedirectResponse
-   {
+    {
         $request->authenticate();
 
         $request->session()->regenerate();
 
-       return redirect()->intended('dashboard');
+        return redirect()->intended('dashboard');
+        return redirect()->intended(RouteServiceProvider::HOME);
     }
 
     /**
@@ -53,11 +36,8 @@ class AuthenticatedSessionController extends Controller
     public function destroy(Request $request): RedirectResponse
     {
         Auth::guard('web')->logout();
-
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
-
         return redirect('/');
     }
 }
