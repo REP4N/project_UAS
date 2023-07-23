afb6ee9d39a1d7d7a3d8da8df7e0c6f9aaf23b54
Hafizh Zikri <hafizh250317@gmail.com>
2023-07-24 01:31:51 +0700

    memperbaiki

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -3,7 +3,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -27,7 +26,6 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerate();
 
         return redirect()->intended('dashboard');
-        return redirect()->intended(RouteServiceProvider::HOME);
     }
 
     /**
