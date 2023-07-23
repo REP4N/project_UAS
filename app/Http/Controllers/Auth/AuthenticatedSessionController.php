70d4a561f2bd0cc4a108d8e49f6b2f5ba99086c5
REP4N <201220041repan@gmail.com>
2023-07-24 00:26:20 +0700

    up

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -1,6 +1,5 @@
 <?php
 namespace App\Http\Controllers\Auth;
-
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
@@ -17,19 +16,40 @@ class AuthenticatedSessionController extends Controller
     {
         return view('auth.login');
     }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+           'email' => 'required',
+           'password' => 'required' 
+
+        ],[
+            'email.required' => 'Email wajib diisi!',
+            'password.required' => 'Password wajib diisi!'
+        ] ); 
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // Authentication passed
+            return redirect()->intended('/dashboard');
+        } else {
+            // Authentication failed
+            return back()->withInput()->withErrors(['email' => 'Invalid credentials']);
+        }
+    }
+
     /**
      * Handle an incoming authentication request.
      */
     public function store(LoginRequest $request): RedirectResponse
     {
         $request->authenticate();
-
         $request->session()->regenerate();
-
-        return redirect()->intended('dashboard');
         return redirect()->intended(RouteServiceProvider::HOME);
     }
-
     /**
      * Destroy an authenticated session.
      */
